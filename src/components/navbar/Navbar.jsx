b9967eee972386a619e92239e5e73bab37fce611
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-21 04:01:52 +0300

    feat: add logo to Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import './navbar.css';
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
