b713327f2ea17858f243f0aab02bd74d0f92d4cf
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-25 08:50:39 +0300

    chore: import brands imgs in one file

---
+++ src/components/brand/import.js
@@ -0,0 +1,7 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopyfiy from '../../assets/shopyfiy.png';
+
+export { google, slack, atlassian, shopyfiy, dropbox };
